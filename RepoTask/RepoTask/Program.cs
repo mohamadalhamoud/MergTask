b01deaf752d3287ed73915ca76c263d4d90a9882
Mohamad Alhamoud <mohamadalhamoud@Mohamads-MacBook-Pro.local>
2022-02-10 22:13:51 -0600

    Add visualstudio code

---
+++ RepoTask/RepoTask/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace RepoTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] array = new[] { 1, 12, 38, 49, 5, 6, 7, 86, 9 };
+            var maxNumber = array.Max();
+            Console.WriteLine("Max Number Of this Array:"+maxNumber);
+        }
+    }
+}
