67f8f3b7e93e80f0c22262719a87adec31ada8dd
Mohamad Alhamoud <mohamadalhamoud@Mohamads-MacBook-Pro.local>
2022-02-10 22:28:16 -0600

    Added some method

--- RepoTask/RepoTask/Program.cs
+++ RepoTask/RepoTask/Program.cs
@@ -5,12 +5,25 @@ namespace RepoTask
 {
     class Program
     {
+        public static int SumEvenNum(int [] arrayNum)
+        {
+            int result = 0;
+            for(int i=0;i<arrayNum.Length;i++)
+            {
+                if(arrayNum[i]%2==0)
+                {
+                    result += arrayNum[i];
+                }
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
            
-            int[] array = new[] { 1, 12, 38, 49, 5, 6, 7, 86, 9 };
+            int[] array = new[] { 1, 12, 8, 7, 5, 6, 9 };
             var maxNumber = array.Max();
             Console.WriteLine("Max Number Of this Array:"+maxNumber);
+            Console.WriteLine(SumEvenNum(array));
         }
     }
 }
